2a74b721374ff122806147ea13ec01f97a81f830
John Taylor <johntaylor@gmail.com>
2021-03-08 14:55:25 -0500

    intial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # nextfreeip
 Find the next IP address that is not listed in DNS
+
+
+## Usage
+
+```shell
+
+nextfreeip 192.168.1.1
+192.168.1.1      device1.example.com.
+192.168.1.2      device2.example.com.
+192.168.1.3      device3.example.com.
+192.168.1.4      device4.example.com.
+192.168.1.5      device5.example.com.
+192.168.1.6 is not in DNS
+
+```
+
+## Notes
+* The program stops searching after it gets to the `.255` address.
---
+++ cmd/nextfreeip/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func usage(pgm string) {
+	fmt.Println()
+	fmt.Println("nextfreeip: Get the next consecutive IP address that is not found in DNS")
+	fmt.Println("https://github.com/jftuga/nextfreeip")
+	fmt.Println()
+	fmt.Println("Usage:")
+	fmt.Printf("%s [ ip-address ]\n", pgm)
+	fmt.Println()
+}
+
+func resolveIP(ip string) bool {
+	allNames, err := net.LookupAddr(ip)
+	if err != nil {
+		if strings.Contains(err.Error(), "DNS name does not exist") {
+			return false
+		}
+		log.Fatalf("Error while looking up: %s\n%s\n", ip, err)
+	}
+	for _, name := range allNames {
+		fmt.Printf("%s\t%s\n", ip, name)
+		return true
+	}
+	return false
+}
+
+// getLastOctet - return the last octet of an IP in numeric format
+func getLastOctet(ip string) int {
+	slots := strings.Split(ip, ".")
+	last := slots[3]
+	i, err := strconv.Atoi(last)
+	if err != nil {
+		log.Fatalf("Unable to convert string value to int: %s\n%s\n", last, err)
+	}
+	return i
+}
+
+// getTriplet - return the first 3 octets of and IP
+func getTriplet(ip string) string {
+	s := strings.Split(ip, ".")
+	return s[0] + "." + s[1] + "." + s[2]
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		usage(os.Args[0])
+		return
+	}
+
+	ip := os.Args[1]
+	triplet := getTriplet(ip)
+	last := getLastOctet(ip)
+
+	found := false
+	i := 0
+	for i = last; i <= 255; i++ {
+		found = resolveIP(fmt.Sprintf("%s.%d", triplet, i))
+		if !found {
+			fmt.Printf("%s.%d is not in DNS\n", triplet, i)
+			return
+		}
+	}
+
+	fmt.Println()
+	fmt.Printf("Stopped searching at: %s.%d\n", triplet, i-1)
+}
